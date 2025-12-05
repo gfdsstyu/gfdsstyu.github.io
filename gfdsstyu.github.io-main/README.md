2c605c9b5ce768c5599ac01326f9bdf2b8698569
Goalapaduck <jjb3624@gmail.com>
2025-12-05 19:12:32 +0900

    Delete gfdsstyu.github.io-main directory

--- gfdsstyu.github.io-main/README.md
+++
@@ -1,4 +0,0 @@
-# gfdsstyu.github.io
-kicpa audit standard study
-
-CC BY-NC-SA 4.0 (Attribution-NonCommercial-ShareAlike)
--- gfdsstyu.github.io-main/js/state/store.js
+++
@@ -1,9 +0,0 @@
-export function createStore() {
-  let state = {};
-  const subs = new Set();
-  return {
-    get: () => state,
-    set: (k, v) => { state = { ...state, [k]: v }; subs.forEach(fn => fn(state)); },
-    subscribe: (fn) => { subs.add(fn); return () => subs.delete(fn); }
-  };
-}
